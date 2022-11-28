24296eb48445f1f546a3dd0b4eee614d771ff204
Zsigil <chickpox@freemail.hu>
2022-11-28 15:37:18 +0100

    soroban number set from pinia store

--- soroban-vue3/src/components/Soroban/SorobanFull.vue
+++ soroban-vue3/src/components/Soroban/SorobanFull.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="flex">
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn />
-    <BeadColumn :digit="7" />
+    <BeadColumn
+      v-for="(digit, i) in store.getSorobanNumberList"
+      :digit="digit"
+      :key="i"
+    />
   </div>
 </template>
 
 <script setup>
+import { useSorobanStore } from "@/stores/soroban";
 import BeadColumn from "./BeadColumn.vue";
+
+const store = useSorobanStore();
 </script>
 
 <style></style>
---
+++ soroban-vue3/src/stores/soroban.js
@@ -0,0 +1,26 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useSorobanStore = defineStore('soroban', {
+  state: () => ({ sorobanNumber: 73, columnNumber: 13, sorobanError: false }),
+  getters: {
+    getSorobanNumberList() {
+      this.sorobanError = false;
+      let zeros = [];
+      let firstList = this.sorobanNumber.toString().split('')
+      if (firstList.length > this.columnNumber) {
+        this.sorobanError = true;
+        return new Array(this.columnNumber).fill(0);
+      }
+      zeros = new Array(this.columnNumber - firstList.length).fill(0);
+      let digitList = firstList.map((d) => d * 1)
+      return [...zeros, ...digitList]
+
+    }
+  },
+  actions: {
+    setSorobanNumber(newValue) {
+      this.sorobanNumber = newValue;
+    }
+  },
+})
